353a2b4afcd05387149ab831bb277917cab5db09
Kjartan Einarsson <kjartanreinarsson@gmail.com>
2025-01-28 16:27:14 -0800

    update to bun and tailwind pure

--- home/next.config.mjs
+++ home/next.config.mjs
@@ -3,8 +3,15 @@
 
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"]
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
+  webpack: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  }
 };
 
 
